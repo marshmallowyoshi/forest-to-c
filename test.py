f29150681f1a6f04ab4289887aec5b17c3beee3e
marshmallowyoshi <97353790+marshmallowyoshi@users.noreply.github.com>
2024-02-27 15:31:07 +0000

    Added docs and cleaned code

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+"""
+Module used to test functionality of converter using the Iris dataset.
+"""
 from sklearn.ensemble import RandomForestClassifier
 import sklearn.datasets as datasets
 import forest_to_c as converter
@@ -9,10 +12,10 @@ rf = RandomForestClassifier()
 rf.fit(x, y)
 converter.forest_to_c(rf, keep_temporary_files=True)
 
-predictions = rf.predict(x)
+# predictions = rf.predict(x)
 
-x_predict = [list(sample)+ [predictions[idx]] for idx, sample in enumerate(x)]
+# x_predict = [list(sample)+ [predictions[idx]] for idx, sample in enumerate(x)]
 
-with open('test.csv', 'w') as f:
-    for sample in x_predict:
-        f.write(','.join([str(x) for x in sample]) + '\n')
+# with open('test.csv', 'w') as f:
+#     for sample in x_predict:
+#         f.write(','.join([str(x) for x in sample]) + '\n')
